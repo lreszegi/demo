7ae3cc3b8ce23186b35c597040b1963f3b2fc388
Laszlo Reszegi <v-lreszegi@expedia.com>
2018-07-03 19:26:10 +0200

    YOLO

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Laszlo Reszegi (v-lreszegi@expedia.com)
+ */
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
